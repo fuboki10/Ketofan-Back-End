34268d5dd11472ce6ec17a7f871bb74e45ed1c7c
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-04-29 21:32:26 +0200

    init server

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const app = require('./app');
+const config = require('config');
+
+const port = process.env.PORT || config.get('PORT') || 3000;
+
+let server = app.listen(port, () =>
+  console.log(`app listening on port ${port}`)
+);
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      console.log('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  console.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
