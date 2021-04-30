ff5d856d5a1d0d333d95da1afc8cd88ef486b56e
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-04-30 09:38:49 +0200

    add morgan and cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const config = require('config');
+const cors = require('cors');
+const morgan = require('./utils/morgan');
 
 const app = express();
 
+const nodeEnv = process.env.NODE_ENV || config.get('NODE_ENV') || 'development';
+
+// logger format
+if (nodeEnv !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+// parse json request body
 app.use(express.json());
+
+// enable cors
+const corsOptions = {
+  exposedHeaders: ['x-auth-token', 'auth-token'],
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', async (req, res) => {
   res.send('Hello world!!');
---
+++ src/utils/morgan.js
@@ -0,0 +1,27 @@
+const morgan = require('morgan');
+const config = require('config');
+const logger = require('./logger');
+
+const nodeEnv = process.env.NODE_ENV || config.get('NODE_ENV');
+morgan.token('message', (req, res) => res.locals.errorMessage || '');
+
+const getIpFormat = () => (nodeEnv === 'production' ? ':remote-addr - :remote-user [:date[clf]]' : '');
+// eslint-disable-next-line max-len
+const successResponseFormat = `${getIpFormat()} ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms`;
+// eslint-disable-next-line max-len
+const errorResponseFormat = `${getIpFormat()} ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms - message: :message`;
+
+const successHandler = morgan(successResponseFormat, {
+  skip: (req, res) => res.statusCode >= 400,
+  stream: { write: (message) => logger.info(message.trim()) },
+});
+
+const errorHandler = morgan(errorResponseFormat, {
+  skip: (req, res) => res.statusCode < 400,
+  stream: { write: (message) => logger.error(message.trim()) },
+});
+
+module.exports = {
+  successHandler,
+  errorHandler,
+};
