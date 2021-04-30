1a11a639b4bbec937d692d3ac545b9ab21467477
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-04-30 03:45:47 +0200

    add logger

---
+++ src/utils/logger.js
@@ -0,0 +1,32 @@
+const winston = require('winston');
+const config = require('config');
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const transports = [
+  new winston.transports.Console({
+    stderrLevels: ['error'],
+  }),
+];
+
+const nodeEnv = process.env.NODE_ENV || config.get('NODE_ENV') || 'development';
+
+const logger = winston.createLogger({
+  level: nodeEnv === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    nodeEnv === 'development'
+      ? winston.format.colorize()
+      : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+  ),
+  transports: transports,
+});
+
+module.exports = logger;
