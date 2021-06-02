b0286a02e5ac80238ebef7f67d2487e14900f924
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-06-02 15:38:23 +0200

    add area service

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,8 +2,9 @@
 import express from 'express';
 import authRouter from './auth.router';
 import verifyRouter from './verify.router';
-import specializationRouter from './specialization.route';
+import specializationRouter from './specialization.router';
 import contactUsRouter from './contactUs.router';
+import areaRouter from './area.router';
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use('/auth', authRouter);
 router.use('/verify', verifyRouter);
 router.use('/specializations', specializationRouter);
 router.use('/contactUs', contactUsRouter);
+router.use('/areas', areaRouter);
 
 export default router;
--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,7 @@ import mailService from './mail.service';
 import verifyService from './verify.service';
 import specializationService from './specialization.service';
 import contactUsService from './contactUs.service';
+import areaService from './area.service';
 
 export {
   authService,
@@ -10,4 +11,5 @@ export {
   verifyService,
   specializationService,
   contactUsService,
+  areaService,
 };
