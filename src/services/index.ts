74f8a065124dabbe806aef3808ca98f8472686b5
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-06-07 20:02:33 +0200

    refactor services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,25 +1,11 @@
-import authService from './auth.service';
-import mailService from './mail.service';
-import verifyService from './verify.service';
-import specializationService from './specialization.service';
-import contactUsService from './contactUs.service';
-import areaService from './area.service';
-import insuranceService from './insurance.service';
-import doctorService from './doctor.service';
-import doctorRequestService from './doctorRequest.service';
-import imageService from './image.service';
-import userService from './user.service';
-
-export {
-  authService,
-  mailService,
-  verifyService,
-  specializationService,
-  contactUsService,
-  areaService,
-  insuranceService,
-  doctorService,
-  doctorRequestService,
-  imageService,
-  userService,
-};
+export * as authService from './auth.service';
+export * as mailService from './mail.service';
+export * as verifyService from './verify.service';
+export * as specializationService from './specialization.service';
+export * as contactUsService from './contactUs.service';
+export * as areaService from './area.service';
+export * as insuranceService from './insurance.service';
+export * as doctorService from './doctor.service';
+export * as doctorRequestService from './doctorRequest.service';
+export * as imageService from './image.service';
+export * as userService from './user.service';
