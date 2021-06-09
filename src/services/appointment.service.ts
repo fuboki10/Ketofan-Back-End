4f13ece5ec69a968d3d033ee913af9db938bff73
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-06-09 14:37:39 +0200

    seeds

--- src/services/appointment.service.ts
+++ src/services/appointment.service.ts
@@ -119,6 +119,8 @@ export const getPatientAppointments = async (
 };
 
 export const remove = async (id:number) => {
+  console.log(id);
+
   const appointment : any = await Appointment.db
     .returning('*')
     .delete()
@@ -127,10 +129,10 @@ export const remove = async (id:number) => {
   if (!appointment || !appointment[0]) { throw new AppError('Appointment with the given id is not found', status.NOT_FOUND); }
 
   // update available booking
-  /* await Booking.db
+  await Booking.db
     .update({ available: true })
     .where({ id: appointment[0].bookingId });
-*/
+
   return appointment[0];
 };
 
