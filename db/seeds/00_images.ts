9145426157544cd9fb6433110e5ef405ff2f0a12
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-06-09 06:25:10 +0200

    update seeds

--- db/seeds/00_images.ts
+++ db/seeds/00_images.ts
@@ -9,5 +9,23 @@ export async function seed(knex: Knex): Promise<void> {
     {
       filename: 'p1.jpg', filepath: 'public/images/p1.jpg', mimetype: 'image/jpeg', size: 18021,
     },
+    {
+      filename: 'p2.jpg', filepath: 'public/images/p2.jpg', mimetype: 'image/jpeg', size: 18021,
+    },
+    {
+      filename: 'p3.jpg', filepath: 'public/images/p3.jpg', mimetype: 'image/jpeg', size: 18021,
+    },
+    {
+      filename: 'p4.jpg', filepath: 'public/images/p4.jpg', mimetype: 'image/jpeg', size: 18021,
+    },
+    {
+      filename: 'p5.jpg', filepath: 'public/images/p5.jpg', mimetype: 'image/jpeg', size: 18021,
+    },
+    {
+      filename: 'p6.jpg', filepath: 'public/images/p6.jpg', mimetype: 'image/jpeg', size: 18021,
+    },
+    {
+      filename: 'p7.jpg', filepath: 'public/images/p7.jpg', mimetype: 'image/jpeg', size: 18021,
+    },
   ]);
 }
