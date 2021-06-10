7f4c91163045f0dd18bff735ed91f421e6fa2eef
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-06-10 16:06:03 +0200

    remove document from auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,18 +4,6 @@ import AppError from '../utils/AppError';
 import { authService } from '../services';
 import { User } from '../models';
 
-/**
- * Authentication middleware
- *
- * @version 1.0.0
- * @throws AppError 401 if no/wrong token passed
- * @author Abdelrahman Tarek
- * @param {Object} req - Express request object
- * @param {Object} res - Express response object
- * @param {Function} next - Express next middleware function
- * @description takes user token to authenticate user
- * @summary User Authentication
- */
 export const authenticate = async (req : Request, res : Response, next : NextFunction) => {
   // getting token and check if it is there
   const [bearer, token] = req?.headers?.authorization?.split ? req.headers.authorization.split(' ') : [undefined];
@@ -37,17 +25,6 @@ export const authenticate = async (req : Request, res : Response, next : NextFun
   return next();
 };
 
-/**
- * @version 1.0.0
- * @throws AppError 403 if user doesn`t have permission
- * @author Abdelrahman Tarek
- * @param {Object} req - Express request object
- * @param {Object} res - Express response object
- * @param {Function} next - Express next middleware function
- * @param {Array<String>} roles authorized roles
- * @description give permission to users based on roles
- * @summary User Authorization
- */
 // eslint-disable-next-line max-len
 export const authorize = (roles : Array<string>) => (req : Request, res : Response, next : NextFunction) => {
   if (!roles.includes(req.user.role)) {
